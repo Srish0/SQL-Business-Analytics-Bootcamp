41972a3af09a5e10cf1f091f2284ec1db70f2b05
srish <144916473+Srish0@users.noreply.github.com>
2026-07-04 23:41:25 +0530

    Add sample data

---
+++ data.sql
@@ -0,0 +1,23 @@
+USE analytics_bootcamp;
+
+INSERT INTO customers (customer_id, customer_name, city, age)
+VALUES
+(1, 'Srishti', 'Chandigarh', 21),
+(2, 'Aman', 'Delhi', 25),
+(3, 'Riya', 'Mumbai', 23),
+(4, 'Karan', 'Pune', 28),
+(5, 'Simran', 'Jaipur', 22),
+(6, 'Rahul', 'Delhi', 24),
+(7, 'Anjali', 'Delhi', 22),
+(8, 'Priya', 'Mumbai', 26),
+(9, 'Rohit', 'Chandigarh', 23);
+
+INSERT INTO orders
+VALUES
+(101,1,'Laptop',60000),
+(102,2,'Phone',30000),
+(103,1,'Keyboard',2500),
+(104,3,'Monitor',15000),
+(105,5,'Mouse',1000),
+(106,9,'Headphones',3500),
+(107,100,'Tablet',25000);
